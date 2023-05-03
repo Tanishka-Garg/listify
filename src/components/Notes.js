272c03993a640fce36e4e299667bb00c2b85c011
Tanishka-Garg <tan27peace@gmail.com>
2023-05-03 22:28:47 +0530

    fetch api connected

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import noteContext from "../context/notes/noteContext";
 import Noteitem from "./Noteitem";
 
@@ -8,16 +8,33 @@ const Notes = () => {
   useEffect(() => {
     getNote();
   }, []);
-  const updateNote = (note) => {
+  const updateNote = (currentNote) => {
     ref.current.click();
+    setNote({
+      etitle: currentNote.title,
+      edescription: currentNote.description,
+      etag: currentNote.tag,
+    });
   };
   const ref = useRef(null);
+  const [note, setNote] = useState({
+    etitle: "",
+    edescription: "",
+    etag: "default",
+  });
+  const handleClick = (e) => {
+    //console.log("updating", note);
+    e.preventDefault();
+  };
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value });
+  };
   return (
     <>
       <button
         ref={ref}
         type="button"
-        className="btn btn-primary"
+        className="btn btn-primary d-none"
         data-bs-toggle="modal"
         data-bs-target="#exampleModalCenter"
       >
@@ -46,7 +63,51 @@ const Notes = () => {
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div className="modal-body">...</div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label htmlFor="etitle" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    aria-label="Sizing example input"
+                    id="etitle"
+                    name="etitle"
+                    value={note.etitle}
+                    aria-describedby="inputGroup-sizing-sm"
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="edescription" className="form-label">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="edescription"
+                    name="edescription"
+                    value={note.edescription}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="etag" className="form-label">
+                    tag
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="etag"
+                    name="etag"
+                    value={note.etag}
+                    onChange={onChange}
+                  />
+                </div>
+              </form>
+            </div>
             <div className="modal-footer">
               <button
                 type="button"
@@ -55,7 +116,11 @@ const Notes = () => {
               >
                 Close
               </button>
-              <button type="button" className="btn btn-primary">
+              <button
+                type="button"
+                onClick={handleClick}
+                className="btn btn-primary"
+              >
                 update note
               </button>
             </div>
